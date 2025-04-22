9d70e371b97634254e102c33cc229f4d3abb0542
Huzi1080 <hanis1@hawk.iit.edu>
2025-04-22 02:18:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("entrybutton");
   const input = document.getElementById("entryinput");
   const output = document.getElementById("textoutput");
 
-  button.addEventListener("click", function() {
+  button.addEventListener("click", function () {
     const userInput = input.value;
 
-    
-    alert("Huzaifa Anis: " + userInput);
+    alert("Huzaifa Anis " + userInput);
 
     output.textContent = userInput;
   });
